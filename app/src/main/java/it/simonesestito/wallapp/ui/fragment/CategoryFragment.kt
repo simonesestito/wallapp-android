943ef4ed57ab7643f2c0399353867a09970a5267
Simone Sestito <simone@simonesestito.com>
2018-07-12 20:03:39 +0200

    Add onSaveInstanceState() support
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/it/simonesestito/wallapp/ui/fragment/CategoryFragment.kt
+++ app/src/main/java/it/simonesestito/wallapp/ui/fragment/CategoryFragment.kt
@@ -2,19 +2,21 @@ package it.simonesestito.wallapp.ui.fragment
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-
 import it.simonesestito.wallapp.R
 import it.simonesestito.wallapp.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.category_fragment.*
 
 class CategoryFragment : Fragment() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var args: CategoryFragmentArgs
+
+    private val categoryArgsKey = "categoryId"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -25,9 +27,15 @@ class CategoryFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        val args = CategoryFragmentArgs.fromBundle(arguments)
+        args = CategoryFragmentArgs.fromBundle(arguments)
         viewModel.getCategoryById(args.categoryId).observe(this, Observer {
             singleCategoryDemoText.text = it.displayName
         })
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(categoryArgsKey, args.categoryId)
+        super.onSaveInstanceState(outState)
     }
 }
