1a0eb17c957dd7c904019a42be0941dc576b69dc
Simone Sestito <simone.dev.help@gmail.com>
2018-08-05 17:00:16 +0200

    [Firebase] Display published only documents, ordered by date
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/it/simonesestito/wallapp/data/repository/CategoryRepository.kt
+++ app/src/main/java/it/simonesestito/wallapp/data/repository/CategoryRepository.kt
@@ -2,7 +2,10 @@ package it.simonesestito.wallapp.data.repository
 
 import androidx.lifecycle.LiveData
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import it.simonesestito.wallapp.FIRESTORE_CATEGORIES
+import it.simonesestito.wallapp.KEY_CREATION_DATE
+import it.simonesestito.wallapp.KEY_PUBLISHED
 import it.simonesestito.wallapp.data.model.Category
 import it.simonesestito.wallapp.livedata.FirestoreLiveCollection
 import it.simonesestito.wallapp.livedata.FirestoreLiveDocument
@@ -14,6 +17,8 @@ object CategoryRepository {
     fun getCategories(): LiveData<List<Category>> {
         val ref = FirebaseFirestore.getInstance()
                 .collection(FIRESTORE_CATEGORIES)
+                .whereEqualTo(KEY_PUBLISHED, true)
+                .orderBy(KEY_CREATION_DATE, Query.Direction.DESCENDING)
 
         return FirestoreLiveCollection(ref).mapList { snap ->
             Category(
