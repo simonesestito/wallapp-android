5d983f15d89d13fb8bcde05cbbf0c4eb9eb9705d
Simone Sestito <simone.dev.help@gmail.com>
2018-08-05 17:00:57 +0200

    Wallpaper: show suggested format in single wallpaper fragment
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/it/simonesestito/wallapp/utils/Utils.kt
+++ app/src/main/java/it/simonesestito/wallapp/utils/Utils.kt
@@ -1,5 +1,6 @@
 package it.simonesestito.wallapp.utils
 
+import android.util.DisplayMetrics
 import android.widget.ImageView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
@@ -8,6 +9,10 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import it.simonesestito.wallapp.GlideApp
 import it.simonesestito.wallapp.R
+import it.simonesestito.wallapp.annotations.FORMAT_16_9
+import it.simonesestito.wallapp.annotations.FORMAT_18_9
+import it.simonesestito.wallapp.annotations.WallpaperFormat
+import it.simonesestito.wallapp.annotations.dimensions
 
 val Any.TAG: String
     get() = this.javaClass.simpleName
@@ -43,3 +48,23 @@ fun ImageView.setFirebaseImage(url: String) = setFirebaseImage(
         FirebaseStorage.getInstance()
                 .getReference(url)
 )
+
+@WallpaperFormat
+fun getSuggestedWallpaperFormat(displayMetrics: DisplayMetrics): String {
+    // Calculate user aspect ratio
+    val userRatio = displayMetrics.widthPixels / displayMetrics.heightPixels.toDouble()
+
+    val formats = arrayOf(FORMAT_16_9, FORMAT_18_9)
+
+    // Find if user display is exactly one of the formats
+    formats.forEach { format ->
+        if (format.dimensions.ratio == userRatio) {
+            return format
+        }
+    }
+
+    // If not exactly one of those formats, pick the best one
+    return formats
+            .sortedBy { format -> Math.abs(format.dimensions.ratio - userRatio) }
+            .first()
+}
