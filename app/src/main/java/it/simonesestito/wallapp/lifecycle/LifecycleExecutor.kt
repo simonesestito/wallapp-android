b9c10c622cda1776201659a45f3b90ab7db51754
Simone Sestito <simone@simonesestito.com>
2018-08-13 09:38:09 +0200

    Add LifecycleExecutor utility class
    Execute an action only when lifecycle is at least started or wait for being in started state
    
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

---
+++ app/src/main/java/it/simonesestito/wallapp/lifecycle/LifecycleExecutor.kt
@@ -0,0 +1,15 @@
+package it.simonesestito.wallapp.lifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+
+
+class LifecycleExecutor(private inline val action: () -> Unit) : LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun executeAction(owner: LifecycleOwner) {
+        owner.lifecycle.removeObserver(this)
+        action()
+    }
+}
