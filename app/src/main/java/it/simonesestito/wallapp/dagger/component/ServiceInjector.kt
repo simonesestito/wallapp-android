4f0bd0b1e0dcd53353e6eec001281c6b9bdd45d8
Simone Sestito <simone@simonesestito.com>
2018-08-14 11:21:01 +0200

    Replace usages of @Singleton scope with @Reusable
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/it/simonesestito/wallapp/dagger/component/ServiceInjector.kt
+++ app/src/main/java/it/simonesestito/wallapp/dagger/component/ServiceInjector.kt
@@ -3,10 +3,8 @@ package it.simonesestito.wallapp.dagger.component
 import dagger.Component
 import it.simonesestito.wallapp.backend.service.PreviewService
 import it.simonesestito.wallapp.dagger.module.ThreadModule
-import javax.inject.Singleton
 
 
-@Singleton
 @Component(modules = [ThreadModule::class])
 interface ServiceInjector {
     fun inject(previewService: PreviewService)
