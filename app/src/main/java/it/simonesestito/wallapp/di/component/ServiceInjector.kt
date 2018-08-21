21e00eee02e9dcf4c12a00e93eeda5b39710c6d9
Simone Sestito <simone@simonesestito.com>
2018-08-21 10:11:16 +0200

    Fix Dagger "non-public modules visibility"
    Signed-off-by: Simone Sestito <simone.dev.help@gmail.com>

--- app/src/main/java/it/simonesestito/wallapp/di/component/ServiceInjector.kt
+++ app/src/main/java/it/simonesestito/wallapp/di/component/ServiceInjector.kt
@@ -3,8 +3,10 @@ package it.simonesestito.wallapp.di.component
 import dagger.Component
 import it.simonesestito.wallapp.backend.service.PreviewService
 import it.simonesestito.wallapp.di.module.ThreadModule
+import javax.inject.Singleton
 
 
+@Singleton
 @Component(modules = [ThreadModule::class])
 interface ServiceInjector {
     fun inject(previewService: PreviewService)
--- app/src/main/java/it/simonesestito/wallapp/utils/Utils.kt
+++ app/src/main/java/it/simonesestito/wallapp/utils/Utils.kt
@@ -5,11 +5,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Build
 import android.view.View
 import android.view.animation.Animation
 import androidx.annotation.ColorInt
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.core.graphics.ColorUtils
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
@@ -219,3 +221,7 @@ fun LifecycleOwner.executeOnReady(action: () -> Unit) {
  */
 inline fun <reified T : ViewModel> Fragment.getViewModel(factory: ViewModelProvider.Factory) =
         ViewModelProviders.of(this, factory)[T::class.java]
+
+fun Context.isConnectivityOnline() =
+        ContextCompat.getSystemService(this, ConnectivityManager::class.java)
+                ?.activeNetworkInfo?.isConnected ?: false
