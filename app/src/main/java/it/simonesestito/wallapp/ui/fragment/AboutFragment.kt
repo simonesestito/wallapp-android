8c03761e22436d7361894b8d779bb7d0c0ee468a
Simone Sestito <simone@simonesestito.com>
2018-09-14 17:56:37 +0200

    Add Social + Email in About, adjust scroll elevation

--- app/src/main/java/it/simonesestito/wallapp/ui/fragment/AboutFragment.kt
+++ app/src/main/java/it/simonesestito/wallapp/ui/fragment/AboutFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.net.toUri
+import androidx.core.widget.NestedScrollView
 import com.google.android.material.snackbar.Snackbar
 import it.simonesestito.wallapp.*
 import it.simonesestito.wallapp.utils.openUrl
@@ -28,31 +29,45 @@ class AboutFragment : AbstractAppFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.authorPortfolioButton1.setOnClickListener {
-            requireContext().openUrl(AUTHOR_1_PORTFOLIO_WEBSITE)
+        view.aboutScrollContentRoot.setOnScrollChangeListener { _: NestedScrollView?, _: Int, y: Int, _: Int, _: Int ->
+            adjustElevation(y)
         }
 
-        view.authorPortfolioButton2.setOnClickListener {
-            requireContext().openUrl(AUTHOR_2_PORTFOLIO_WEBSITE)
-        }
+        view.apply {
+            authorPortfolioButton1.setOnClickListener {
+                context?.openUrl(AUTHOR_1_PORTFOLIO_WEBSITE)
+            }
 
-        view.authorMailButton1.setOnClickListener {
-            sendEmail(AUTHOR_1_MAIL)
-        }
+            authorPortfolioButton2.setOnClickListener {
+                context?.openUrl(AUTHOR_2_PORTFOLIO_WEBSITE)
+            }
 
-        view.authorMailButton2.setOnClickListener {
-            sendEmail(AUTHOR_2_MAIL)
-        }
+            authorMailButton1.setOnClickListener {
+                sendEmail(AUTHOR_1_MAIL)
+            }
 
-        view.aboutFeedbackPlayButton.setOnClickListener {
-            requireContext().openUrl(GOOGLE_PLAY_LINK)
+            authorMailButton2.setOnClickListener {
+                sendEmail(AUTHOR_2_MAIL)
+            }
+
+            aboutFeedbackPlayButton.setOnClickListener {
+                context?.openUrl(GOOGLE_PLAY_LINK)
+            }
+
+            aboutFeedbackMailButton.setOnClickListener {
+                sendEmail(AUTHOR_1_MAIL)
+            }
+
+            aboutFeedbackSocialButton.setOnClickListener {
+                context?.openUrl(TWITTER_LINK)
+            }
         }
     }
 
     private fun sendEmail(address: String) {
         val intent = Intent(Intent.ACTION_SENDTO, "mailto:$address".toUri())
         try {
-            startActivity(intent)
+            activity?.startActivity(intent)
         } catch (e: ActivityNotFoundException) {
             e.printStackTrace()
             Snackbar.make(view!!, R.string.activity_not_found_error, Snackbar.LENGTH_LONG).show()
