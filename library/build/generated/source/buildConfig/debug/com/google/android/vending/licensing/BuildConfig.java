3b5362959cfafafcd5a119f6091806e5ecca4adb
Simone Sestito <simone@simonesestito.com>
2018-09-17 17:14:29 +0200

    Add unversioned LVL files

---
+++ library/build/generated/source/buildConfig/debug/com/google/android/vending/licensing/BuildConfig.java
@@ -0,0 +1,18 @@
+/*
+ * This file is part of WallApp for Android.
+ * Copyright © 2018 Simone Sestito. All rights reserved.
+ */
+
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.android.vending.licensing;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.android.vending.licensing";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.5";
+}
